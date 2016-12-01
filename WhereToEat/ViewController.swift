c26a86949459674470fae3ea1b38d457dfd1f662
Mélanie Godard <melanie.godard@gmail.com>
2016-12-01 16:27:18 +0100

    Initial Commit

---
+++ WhereToEat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereToEat
+//
+//  Created by Mélanie Godard on 01/12/2016.
+//  Copyright © 2016 Wellcut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
